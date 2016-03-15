6d513635442fe1c77bdc4cb64fc02e2efc6da3ce
Abhinav Shankaranarayanan Venkataraman <abhinav.shankaranarayanan@c6s304mac06.fib.upc.es>
2016-03-15 12:59:06 +0100

    Python Trial

---
+++ trial.py
@@ -0,0 +1,25 @@
+
+
+class MyClass:
+  """ Simple Class """
+  i = 5
+  def __init__(self):
+    print "Constructor Called"
+  def f(self):
+    return "Hello World"
+
+
+def fun():
+   print "Test"
+
+y = MyClass()
+print "Here " + str(y.i)
+y.i= 10
+print "y 2 " + str(y.i)
+z = MyClass()
+print z.i
+print "I am Here"
+z = y.f()
+print z
+
+fun()
